f60c87be60f1986de657e8f7d0e7c4ed074c1c06
henilshahdev <henil.601@gmail.com>
2025-11-13 20:18:00 +0530

    chore: remove proxy middleware

--- proxy.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import { captureRegistryEvent } from "@wandry/analytics-sdk";
-
-export function proxy(request: NextRequest) {
-  captureRegistryEvent(request, process.env.WANDRY_ANALYTICS_TOKEN || "");
-  return NextResponse.next();
-}
